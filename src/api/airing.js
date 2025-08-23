7b291f7b4014da119778cbb20ff91ffeaa32e419
Mahmoud <lamm.mah2002@gmail.com>
2025-08-23 22:21:54 +0200

    add Airing anime listing page

---
+++ src/api/airing.js
@@ -0,0 +1,42 @@
+
+
+import { useState, useEffect } from "react";
+import Aff_anime from "./affichage";
+    
+export default function Airing() {
+  const [anime, setanime] = useState([]);
+
+  useEffect(()=>{
+    fetch(`https://api.jikan.moe/v4/top/anime?filter=airing`) 
+    .then((d)=>d.json())
+    .then((d)=>setanime((d.data)))
+
+  } ,[])
+ 
+
+  return (
+    <div className="anime-grid">
+            {
+             anime.filter((o)=> o.rating && 
+                o.rating !== "R - 17+ (violence & profanity)" &&
+                o.rating !== "R+ - Mild Nudity" &&
+                o.rating !== "Rx - Hentai" &&
+                o.rating !== "OVA"
+          )
+                .map((p)=>{
+                    return (
+                        <div  >
+                           
+                            <Aff_anime anime={p} />
+
+                                 
+                        </div>
+                    );
+
+                })
+            }
+
+        </div>
+    
+  );
+}
