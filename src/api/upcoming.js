ad7923b33f213d742958d07bfc560a020d89394b
Mahmoud <lamm.mah2002@gmail.com>
2025-08-26 22:05:54 +0200

    add Upcoming anime listing page

---
+++ src/api/upcoming.js
@@ -0,0 +1,44 @@
+
+
+
+
+import { useState, useEffect } from "react";
+import Aff_anime from "./affichage";
+    
+export default function Upcoming() {
+  const [anime, setanime] = useState([]);
+
+  useEffect(()=>{
+    fetch(`https://api.jikan.moe/v4/top/anime?filter=upcoming`) 
+    .then((d)=>d.json())
+    .then((d)=>setanime((d.data)))
+
+  } ,[])
+ 
+
+  return (
+    <div className="anime-grid">
+            {
+             anime.filter((o)=> o.rating && 
+                o.rating !== "R - 17+ (violence & profanity)" &&
+                o.rating !== "R+ - Mild Nudity" &&
+                o.rating !== "Rx - Hentai" &&
+                o.rating !== "OVA"
+          )
+                .map((p)=>{
+                    return (
+                        <div  >
+                           
+                            <Aff_anime anime={p} />
+
+                                 
+                        </div>
+                    );
+
+                })
+            }
+
+        </div>
+    
+  );
+}
